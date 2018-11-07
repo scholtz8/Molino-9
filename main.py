09ccf9b68ae279bca53a9199f712bf451957b013
Cristian Scholtz Chamy <cristian.scholtz@gmail.com>
2018-11-07 12:05:26 -0300

    4 tipos de tablero, modos de jeugos con 3,5,6,7 o 9 piezas

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from partida import Partida
 
 print('MOLINO 9\n')
 
-piezas_posibles = [3,4,5,6,7,9]
+piezas_posibles = [3,5,6,7,9]
 numero = int(input('seleccione el número de piezas [3, 5, 6, 7 ó 9]:'))
 if numero not in piezas_posibles:
     print('numero invalido')
